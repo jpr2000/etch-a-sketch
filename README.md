8e86c2b8e50a7d429ef22110199db43d860af4b4
Trey <treyroyalt@gmail.com>
2019-09-03 10:37:12 -0500

    Added drawing functionality and party mode

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+Using DOM manipulation to create an etch-a-sketch web app.
---
+++ script.js
@@ -0,0 +1,61 @@
+function createBoxes (s) {
+    let body = document.querySelector("body");
+    let biggerContainer = document.createElement("div");
+    biggerContainer.setAttribute("id", "biggerContainer");
+    body.appendChild(biggerContainer);
+    let container = document.createElement("div");
+    container.setAttribute("id", "container");
+    
+    let resetButton = document.createElement("button")
+    resetButton.textContent = "Reset";
+    biggerContainer.appendChild(resetButton);
+
+    let partyButton = document.createElement("button")
+    partyButton.textContent = "Party Mode";
+    biggerContainer.appendChild(partyButton);
+    
+    
+    for (let i = 0; i < s ** 2; i++) {
+        container.setAttribute("style", `grid-template: repeat(${s}, 1fr) / repeat(${s}, 1fr);`)
+        let div = document.createElement("div");
+        div.setAttribute("class", "box")
+        div.onmouseover = () => div.style.background = "black";
+        container.appendChild(div);
+    }
+    
+    biggerContainer.appendChild(container);
+    
+    
+    boxes = document.querySelectorAll(".box");
+    
+    function clearBoxes() {
+        boxes.forEach((box) => {
+            box.style.background = "white";
+        });
+    }
+    
+    resetButton.addEventListener("click", () => {
+        clearBoxes()
+        let boxesPerSide = prompt("How many squares per side?");
+        createBoxes(boxesPerSide);
+        biggerContainer.remove();
+    });
+
+    partyButton.addEventListener("click", () => {
+        boxes.forEach((box) => {
+            box.onmouseover = () => box.style.background = getRandomColor();
+        });
+    });
+}
+
+function getRandomColor() {
+    let selection = "0123456789ABCDEF";
+    selection = selection.split('');
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += selection[Math.floor(Math.random() * selection.length)];
+    }
+    return color;
+}
+
+createBoxes(8);
